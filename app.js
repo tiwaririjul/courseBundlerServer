3633999ba5fb6423a0bf6558c7069107dbb008ca
tiwaririjul <tiwaririjul7@gmail.com>
2023-04-08 01:46:04 +0530

    initial commit

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ import express from "express";
 import cookieParser from "cookie-parser";
 import { config } from "dotenv";
 import ErrorMiddleware from "./middlewares/Error.js";
+import cors from "cors";
 
 config({ path: "./config/config.env" });
 
@@ -15,6 +16,13 @@ app.use(
   })
 );
 app.use(cookieParser());
+app.use(
+  cors({
+    origin: process.env.FRONTEND_URL,
+    credentials: true,
+    methods: ["GET", "POST", "PUT", "DELETE"],
+  })
+);
 
 //Importing and using routes
 import course from "./routes/courseRouter.js";
@@ -27,5 +35,11 @@ app.use("/api/v1", payment);
 app.use("/api/v1", other);
 
 export default app;
+
+app.get("/", (req, res) => {
+  res.send(
+    `<h1>Site is Working. Click <a href=${process.env.FRONTEND_URL} to visit frontend></a></h1>`
+  );
+});
 
 app.use(ErrorMiddleware);
